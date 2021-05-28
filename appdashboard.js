892f8033e25ac4df6a142a70a23104cd34d9919e
Tavish Sharma <tavisharma.py@gmail.com>
2021-05-28 17:23:50 +0530

    Add files via upload

---
+++ appdashboard.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+function myFunctionone() {
+  var x = document.getElementById("t1");
+  if (x.style.display === "none") {
+    x.style.display = "inline-block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunctiontwo() {
+  var x = document.getElementById("t2");
+  if (x.style.display === "none") {
+    x.style.display = "inline-block";
+  } else {
+    x.style.display = "none";
+  }
+}
